739f478d2967754ee50da29a41c37144bb1427f0
Eman <emanelhalwany1@.com>
2019-10-06 21:47:37 +0200

    change hover dropdown menu & المزيد button & responsive tabs and 2 articles in md screen

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 // taps 
-$(document).ready(function()
-{ 
-    // tabs
-	$("#years li").click(function()
-	{
-		var myID = $(this).attr("id");
-		$(this).removeClass("inactive").siblings().addClass("inactive");
-		$(".years-content").hide();
-		$("."+myID+"-content").show("slow");
-	});
+$(document).ready(function () {
+  // tabs
+  $("#years li").click(function () {
+    var myID = $(this).attr("id");
+    $(this).removeClass("inactive").siblings().addClass("inactive");
+    $(".years-content").hide();
+    $("." + myID + "-content").toggle(1000);
+  });
 
-    // more button
-        $(".toggle").hide();
-        $(".click").click(function(){
-        $(".toggle").toggle();
+  // more button
+  $(".toggle").hide();
+  $(".click").click(function () {
+    $(".toggle").toggle();
+    if ($(this).text() == "المزيد") {
+      $(this).text("الاقل");
+    } else {
+      $(this).text("المزيد");
+    }
+
   });
 });
 
 
 // stars js
-const ratings = 
+const ratings =
 {
-    phoneName1: 4.7,
-    phoneName2: 4.7,
-    phoneName3: 4.7
+  phoneName1: 4.7,
+  phoneName2: 4.7,
+  phoneName3: 4.7
 
 }
 // Total Stars
@@ -38,8 +42,7 @@ function getRatings() {
 
     // Round to nearest 10
     const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-    console.log(typeof(starPercentageRounded));
-
+    
     // Set width of stars-inner to percentage
     document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
   }
